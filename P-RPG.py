4017a36888e65f858e9a6f23bddfe931c935a721
jaszko2085 <74295548+jaszko2085@users.noreply.github.com>
2021-04-06 12:45:15 +0200

    rpg in py

---
+++ P-RPG.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+yname = input("Podaj swoje imie: ")
+yage = int(input("Podaj swój wiek: "))
+b = 5
+if yage == b:
+    print("masz za mało lat")
+elif yage > b:
+    print("ok")
+elif yage < b:
+    print("masz za mało lat")
+
+
+
+print("OK {yname} teraz stwóż swojego bochatera! ".format(yname=yname, yage=yage))
+
+name = input("Nazwij swojego bochatera: ")
+age = int(input("Podaj jego wiek: "))
+print ('czekaj.')
+print ("czekaj..")
+print ("czekaj...")
+print ("OK bochater {name} został utworzony pomyślnie! {name} ma {age} lat ".format(name=name, age=age))
+
+print ("{name} spotyka pająka!".format(name=name))
+print ("co {name} ma zrobić?".format(name=name))
+print ("ataki:")
+print ('bum, BUUUUUUUM')
+w = int(input("1/2: "))
+bum = 1
+buum = 2 
+if w == 1:
+    print("przegrałeś")
+if w == 2:
+    print("wygrałeś")
+
+print ("{name} spotyka muche!".format(name=name))
+print ("co {name} ma zrobić?".format(name=name))
+print ("ataki:")
+print ('bum, BUUUUUUUM')
+w = int(input("1/2: "))
+bum = 1
+buum = 2 
+if w == 2:
+    print("przegrałeś")
+if w == 1:
+    print("wygrałeś")
+
+print ("{name} spotyka jaszczurkę?".format(name=name))
+print ("co {name} ma zrobić?".format(name=name))
+print ("ataki:")
+print ('bum, BUUUUUUUM')
+w = int(input("1/2: "))
+bum = 1
+buum = 2 
+if w == 1:
+    print("przegrałeś")
+if w == 2:
+    print("wygrałeś")
+
+print ("{name} spotyka potwora!".format(name=name))
+print ("co {name} ma zrobić?".format(name=name))
+print ("ataki:")
+print ('bum, BUUUUUUUM')
+w = int(input("1/2: "))
+bum = 1
+buum = 2 
+if w == 1:
+    print("przegrałeś")
+if w == 2:
+    print("wygrałeś")
+
+print ("{name} spotyka smoka!".format(name=name))
+print ("co {name} ma zrobić?".format(name=name))
+print ("ataki:")
+print ('bum, BUUUUUUUM')
+w = int(input("1/2: "))
+bum = 1
+buum = 2 
+if w == 1:
+    print("przegrałeś")
+if w == 2:
+    print("wygrałeś")
+
+print ("wygrałeś!")
+print ("wszystko: ja")
+print ("koniec")
+
+
+
+
+
+
